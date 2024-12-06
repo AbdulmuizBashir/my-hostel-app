aee53de53d98b3785d37d0c305d9695a1e252a32
AbdulmuizBashir <abdulmuizbashir@gmail.com>
2024-12-06 18:22:52 +0100

    Update register_page.dart

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -86,178 +86,180 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "My Hostel",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 35,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "My Hostel",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 35,
+                  ),
                 ),
-              ),
-              const Text(
-                "Hello again",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 20,
+                const Text(
+                  "Hello again",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    CustomTextfield(
-                      controller: nameController,
-                      hintText: "Name",
-                    ),
-                    const SizedBox(height: 10),
-                    CustomTextfield(
-                      controller: emailController,
-                      hintText: "Email",
-                    ),
-                    const SizedBox(height: 10),
-                    CustomTextfield(
-                      controller: phoneNumberController,
-                      hintText: "Phone Number",
-                    ),
-                    const SizedBox(height: 10),
-                    CustomTextfield(
-                      controller: programController,
-                      hintText: "Program",
-                    ),
-                    const SizedBox(height: 10),
-                    CustomTextfield(
-                      controller: regNumberController,
-                      hintText: "Registration Number",
-                    ),
-                    const SizedBox(height: 10),
-
-                    // Password field with obscure text toggle
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: _isPasswordObscured,
-                      decoration: InputDecoration(
-                        hintText: "Password",
-                        filled: true,
-                        fillColor: const Color(0xffE5E5E5),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide: BorderSide.none,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide: const BorderSide(color: Colors.black),
-                        ),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isPasswordObscured
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isPasswordObscured = !_isPasswordObscured;
-                            });
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      CustomTextfield(
+                        controller: nameController,
+                        hintText: "Name",
                       ),
-                    ),
-                    const SizedBox(height: 10),
-
-                    // Confirm Password field with obscure text toggle
-                    TextField(
-                      controller: _conPasswordController,
-                      obscureText: _isConfirmPasswordObscured,
-                      decoration: InputDecoration(
-                        hintText: "Confirm Password",
-                        filled: true,
-                        fillColor: const Color(0xffE5E5E5),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide: BorderSide.none,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide: const BorderSide(color: Colors.black),
-                        ),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isConfirmPasswordObscured
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isConfirmPasswordObscured =
-                                  !_isConfirmPasswordObscured;
-                            });
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
+                      const SizedBox(height: 10),
+                      CustomTextfield(
+                        controller: emailController,
+                        hintText: "Email",
                       ),
-                    ),
+                      const SizedBox(height: 10),
+                      CustomTextfield(
+                        controller: phoneNumberController,
+                        hintText: "Phone Number",
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextfield(
+                        controller: programController,
+                        hintText: "Program",
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextfield(
+                        controller: regNumberController,
+                        hintText: "Registration Number",
+                      ),
+                      const SizedBox(height: 10),
 
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, bottom: 10),
-                      child: GestureDetector(
-                        onTap: register,
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Colors.black,
+                      // Password field with obscure text toggle
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: _isPasswordObscured,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          filled: true,
+                          fillColor: const Color(0xffE5E5E5),
+                          enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(50),
+                            borderSide: BorderSide.none,
                           ),
-                          child: const Center(
-                            child: Text(
-                              "Register",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: const BorderSide(color: Colors.black),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isPasswordObscured
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isPasswordObscured = !_isPasswordObscured;
+                              });
+                            },
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Confirm Password field with obscure text toggle
+                      TextField(
+                        controller: _conPasswordController,
+                        obscureText: _isConfirmPasswordObscured,
+                        decoration: InputDecoration(
+                          hintText: "Confirm Password",
+                          filled: true,
+                          fillColor: const Color(0xffE5E5E5),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: const BorderSide(color: Colors.black),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isConfirmPasswordObscured
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isConfirmPasswordObscured =
+                                    !_isConfirmPasswordObscured;
+                              });
+                            },
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 10),
+                        child: GestureDetector(
+                          onTap: register,
+                          child: Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Register",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "Already have an account?",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            fontSize: 15,
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: widget.onTap,
-                          child: const Text(
-                            " Login now",
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Already have an account?",
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              color: Colors.blue,
+                              color: Colors.black,
                               fontSize: 15,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
+                          GestureDetector(
+                            onTap: widget.onTap,
+                            child: const Text(
+                              " Login now",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
